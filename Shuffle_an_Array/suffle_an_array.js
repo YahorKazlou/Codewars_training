dd68ca6c1e083579725c6d94a88bfa752bfd3842
Yahor Kazlou <yahor.kazlou.94@gmail.com>
2024-10-02 17:08:26 +0300

    Added Fisher and Yates shuffle metod

---
+++ Shuffle_an_Array/suffle_an_array.js
@@ -0,0 +1,10 @@
+function shuffleArray(array) {
+  for (let i = array.length - 1; i >= 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+}
+
+let arr = [2, 11, 37, 42];
+shuffleArray(arr);
+console.log(arr);
