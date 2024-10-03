0d4193b6cac1e827a2ac97acfe2b33667b6dd6f1
Yahor Kazlou <yahor.kazlou.94@gmail.com>
2024-10-03 13:02:42 +0300

    Finished sum_of_array_averajes.js

--- Sum_of_Array_Averages/sum_of_array_averages.js
+++ Sum_of_Array_Averages/sum_of_array_averages.js
@@ -1,19 +1,15 @@
-function sumAverage(array) {
-  let totalSum = 0;
+const sumAverage = (arr) => {
+  let result;
 
-  for (let subArr of array) {
+  result = 0;
+  for (let subArr of arr) {
     let sum = subArr.reduce((a, b) => a + b, 0);
     let average = sum / subArr.length;
 
-    totalSum += average;
+    result += average;
   }
 
-  return Math.floor(totalSum);
-}
+  result = Math.floor(result);
 
-let arr = [
-  [1, 2, 3],
-  [4, 5, 6],
-  [7, 8],
-];
-console.log(sumAverage(arr));
+  return result;
+};
