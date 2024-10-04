8c4f2d74acd2feab03e495f9f6bd3cefcd0c3655
Yahor Kazlou <yahor.kazlou.94@gmail.com>
2024-10-04 19:06:43 +0300

    Added welcome.js

---
+++ Welcome/welcome.js
@@ -0,0 +1,28 @@
+function greet(language) {
+  const languagesDB = {
+    english: "Welcome",
+    czech: "Vitejte",
+    danish: "Velkomst",
+    dutch: "Welkom",
+    estonian: "Tere tulemast",
+    finnish: "Tervetuloa",
+    flemish: "Welgekomen",
+    french: "Bienvenue",
+    german: "Willkommen",
+    irish: "Failte",
+    italian: "Benvenuto",
+    latvian: "Gaidits",
+    lithuanian: "Laukiamas",
+    polish: "Witamy",
+    spanish: "Bienvenido",
+    swedish: "Valkommen",
+    welsh: "Croeso",
+    belarusian: "Вітаем",
+  };
+
+  if (!language) {
+    return "IP_ADDRESS_REQUIRED";
+  }
+
+  return languagesDB[language.toLowerCase()] || "Welcome";
+}
